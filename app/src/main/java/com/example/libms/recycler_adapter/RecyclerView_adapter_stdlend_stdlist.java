1b9954ea92e6420aa3a95deed4a7839af21b4517
Parash Gurung <pgrg117@gmail.com>
2020-08-20 23:48:48 +0545

    stduent lend book

--- app/src/main/java/com/example/libms/recycler_adapter/RecyclerView_adapter_stdlend_stdlist.java
+++ app/src/main/java/com/example/libms/recycler_adapter/RecyclerView_adapter_stdlend_stdlist.java
@@ -1,23 +1,35 @@
 package com.example.libms.recycler_adapter;
 
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.libms.ConstantValues;
+import com.example.libms.JsonParsers;
+import com.example.libms.LoginActivity;
+import com.example.libms.MainActivity;
 import com.example.libms.R;
 import com.example.libms.model.StudentModel;
 import com.google.android.material.animation.Positioning;
 import com.squareup.picasso.Picasso;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
 import java.util.List;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class RecyclerView_adapter_stdlend_stdlist extends RecyclerView.Adapter<RecyclerView_adapter_stdlend_stdlist.ViewHolder> {
@@ -26,12 +38,19 @@ public class RecyclerView_adapter_stdlend_stdlist extends RecyclerView.Adapter<R
     private ItemClickListener mClickListener;
     List<StudentModel> stdList;
     Context context;
+    JsonParsers jsonparser;
+    AlertDialog.Builder builder;
 
     public RecyclerView_adapter_stdlend_stdlist(Context context, List<StudentModel> stdList, String pid){
         this.context = context;
         this.stdList = stdList;
         this.pid = pid;
+        jsonparser = new JsonParsers();
         this.mInflater = LayoutInflater.from(context);
+
+        builder = new AlertDialog.Builder(context);
+        builder.setTitle("Lend Book? ");
+        builder.setMessage("Are you sure?");
 
     }
 
@@ -69,9 +88,24 @@ public class RecyclerView_adapter_stdlend_stdlist extends RecyclerView.Adapter<R
             mfaculty = (TextView) itemView.findViewById(R.id.facultyname_recycler_lend);
             mSemester = (TextView) itemView.findViewById(R.id.semester_recycler_lend);
             mEmail = (TextView) itemView.findViewById(R.id.email_recycler_lend);
-            mLendBtn = (Button) itemView.findViewById(R.id.lendbook_btn_home);
+            mLendBtn =  itemView.findViewById(R.id.lendbook_btn_home);
             mStudentImage = (ImageView) itemView.findViewById(R.id.ImageForItem_recycler_lend);
             mPhoneno = (TextView) itemView.findViewById(R.id.phoneno_recycler_lend);
+            mLendBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+
+                    builder.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+
+                        public void onClick(DialogInterface dialog, int which) {
+                            new loginasync().execute(pid,getItem(getAdapterPosition()));
+                        }
+                    });
+                    AlertDialog alert = builder.create();
+                    alert.show();
+                }
+            });
             setClickListener(mClickListener);
             itemView.setOnClickListener(this);
         }
@@ -103,4 +137,70 @@ public class RecyclerView_adapter_stdlend_stdlist extends RecyclerView.Adapter<R
     public interface ItemClickListener {
         void onItemClick( int position);
     }
+
+
+    public class loginasync extends AsyncTask<String,String,String> {
+        int FLAG;
+        JSONObject jsonObject;
+
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+
+            String pid = strings[0];
+            String sid = strings[1];
+            HashMap<String,String> datas = new HashMap<>();
+            datas.put("pid", pid);
+            datas.put("sid", sid);
+
+            jsonObject = jsonparser.registerUser("http://"+ConstantValues.ipaddress+"/LibMS/setStudentBookRel/", datas);
+
+            try{
+                if(jsonObject == null){
+                    FLAG = 1;
+                }else if(jsonObject.getString("status").equals("success")){
+                    FLAG = 2;
+
+                }else{
+                    FLAG = 3;
+                }
+            }catch(JSONException e){
+                FLAG = 3;
+            }
+            return null;
+
+
+        }
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            if(FLAG == 1){
+
+            }else if(FLAG == 2){
+                try {
+                    int uid = jsonObject.getInt("uid");
+                    String name = jsonObject.getString("name");
+                    System.out.println(name);
+                }
+                catch (JSONException es){
+                    System.out.println(es);
+                }
+
+            }else if(FLAG == 3){
+
+
+            }else{
+
+            }
+
+        }
+
+    }
+
 }
